cd896da5a91b0b87fbbde30c7717ac266b360e69
Ostap Onyshkiv <prostoostap58@gmail.com>
2022-03-30 20:09:14 +0300

    added files

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_8.1.6/8.1.6.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[101] = ",--,--,,,-asda--,-,";
+			char str2[16] = "*-*-,,*asda--*,";
+			char* dest = new char[16];
+			dest = Change(str);
+			for (int i = 0; i < 15; i++)
+			{
+				Assert::AreEqual(dest[i], str2[i]);
+			}
+		}
+	};
+}
